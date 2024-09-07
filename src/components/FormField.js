2ee54d45ae27ca4c9dbde854950895f5ab3dba5d
ybmin1 <yebin.min1@gmail.com>
2024-09-07 18:55:18 +0900

    update based on feedback

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -3,28 +3,26 @@ const edit = "Edit";
 
 function FormField({ fieldName, onShowModal, value }) {
   return (
-    <>
-      <div className="p-4 flex">
-        <div className="w-full">
-          <span className="w-1/2 text-sm inline-block">{fieldName}: </span>
-          <span className="w-1/2 text-sm inline-block text-green-800">
-            {value}
-          </span>
-        </div>
-        <button
-          onClick={() => onShowModal(fieldName, "add")}
-          className="px-3 text-sm text-green-700 border rounded border-solid border-green-600 mr-1"
-        >
-          {add}
-        </button>
-        <button
-          onClick={() => onShowModal(fieldName, "edit")}
-          className="px-3 text-sm text-green-700 border rounded border-solid border-green-600"
-        >
-          {edit}
-        </button>
+    <div className="p-4 flex">
+      <div className="w-full">
+        <span className="w-1/2 text-sm inline-block">{fieldName}: </span>
+        <span className="w-1/2 text-sm inline-block text-green-800">
+          {value}
+        </span>
       </div>
-    </>
+      <button
+        onClick={() => onShowModal(fieldName, "add")}
+        className="px-3 text-sm text-green-700 border rounded border-solid border-green-600 mr-1"
+      >
+        {add}
+      </button>
+      <button
+        onClick={() => onShowModal(fieldName, "edit")}
+        className="px-3 text-sm text-green-700 border rounded border-solid border-green-600"
+      >
+        {edit}
+      </button>
+    </div>
   );
 }
 
